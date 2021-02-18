5a1d393af1af41ec8707005b0f5bf009ba283ea6
Fedorov Mikhail <fedorov.mikhail.s@gmail.com>
2021-02-18 16:51:32 +0300

    Make some basic tests for Point and Vector.

--- geometry/src/lib.rs
+++ geometry/src/lib.rs
@@ -1,5 +1,5 @@
 #![allow(unused)]
-
+#[derive(Debug)]
 pub struct Point(f64, f64, f64);
 struct Vector(f64, f64, f64);
 
@@ -15,14 +15,13 @@ impl Point {
         panic!("Not implemented!");
     }
 
-    fn shift_by(&self, sift: &Vector) -> Point {
+    fn shift_by(&self, shift: &Vector) -> Point {
         panic!("Not implemented!");
-
     }
 }
 
 impl Vector {
-    fn add(&self, other: &Vector) -> Point {
+    fn add(&self, other: &Vector) -> Vector {
         panic!("Not implemented!");
     }
 
@@ -73,3 +72,61 @@ impl Vector {
 
 // struct Sphere {...}
 // Хотим поддержку шариков ведь, так?)
+
+
+#[cfg(test)]
+mod tests {
+    use crate::Vector;
+    use crate::Point;
+
+    #[test]
+    fn zero_distance_between_point_and_itself() {
+        let point = Point(1., 4., 5.);
+        assert_eq!(point.distance(&point), 0.0);
+    }
+
+    #[test]
+    fn distance_is_positive() {
+        let point = Point(1., 4., 5.);
+        let other_point = Point(2., 1., -4.);
+        assert!(point.distance(&other_point) > 0.0);
+    }
+
+    #[test]
+    fn distance_is_symmetric() {
+        let point = Point(1., 4., 5.);
+        let other_point = Point(2., 1., -4.);
+        assert_eq!(point.distance(&other_point), other_point.distance(&point));
+    }
+
+    #[test]
+    fn triangle_inequality() {
+        let a_point = Point(1., 4., 5.);
+        let b_point = Point(2., 1., -4.);
+        let c_point = Point(2., -1., 2.);
+        assert!(a_point.distance(&b_point) +  b_point.distance(&c_point) >= a_point.distance(&c_point));
+    }
+
+    #[test]
+    fn distance_gives_right_answer() {
+        let a_point = Point(1., 1., 2.);
+        let b_point = Point(1., 1., 0.);
+        assert_eq!(a_point.distance(&b_point), 2.);
+        let a_point = Point(1., 1., 2.);
+        let b_point = Point(1., 4., 2.);
+        assert_eq!(a_point.distance(&b_point), 3.);
+        let a_point = Point(7., 1., 0.);
+        let b_point = Point(1., 1., 0.);
+        assert_eq!(a_point.distance(&b_point), 6.);
+    }
+
+    #[test]
+    fn zero_vector() {
+        let point = Point(1., 5., 6.);
+        let vector = Vector(0., 0., 0.);
+        assert_eq!(point.0, point.shift_by(&vector).0);
+        assert_eq!(point.1, point.shift_by(&vector).1);
+        assert_eq!(point.2, point.shift_by(&vector).2);
+        assert_eq!(0., vector.norm());
+    }
+}
